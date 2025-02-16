293ee20c8c0a5b55a87867cadcbf099aa39f4522
bartoszwmalec <bartosz.malec@yahoo.pl>
2025-02-16 19:30:41 +0100

    Update custom_api.py

--- CustomAPI/custom_api.py
+++ CustomAPI/custom_api.py
@@ -1,9 +1,25 @@
 from pyspark.sql.datasource import DataSource, DataSourceStreamReader, InputPartition
-import re
+from pyspark.sql.types import StructType
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Iterator, List, Sequence, Tuple, Type, Union, TYPE_CHECKING
+# from pyspark.sql.functions import spark_partition_id
+import requests
+from datetime import datetime
+from pyspark.errors import PySparkNotImplementedError
 
+"""
+Requirements:
+PySpark custom data sources are in Public Preview in Databricks Runtime 15.2 and above. Streaming support is available in Databricks Runtime 15.3 and above.
+
+References:
+# https://www.youtube.com/watch?v=Q7GdpqYlBiI "Introducing the New Python Data Source API for Apache Spark™"
+# https://github.com/apache/spark/blob/0d7c07047a628bd42eb53eb49935f5e3f81ea1a1/python/pyspark/sql/datasource.py
 # https://learn.microsoft.com/en-us/azure/databricks/pyspark/datasources
 # https://community.databricks.com/t5/technical-blog/enhancing-the-new-pyspark-custom-data-sources-streaming-api/ba-p/75538
 
+Problems:
+1. Where to define a method for token retrieval?
+"""
 
 class CustomAPI(DataSource):
     """
@@ -12,23 +28,76 @@ class CustomAPI(DataSource):
 
     @classmethod
     def name(cls):
-        return "Custom_API_Source"
+        # Returned name would be used as a format name in commands like "spark.read.format(CustomAPI)" or "spark.write.format(CustomAPI)"
+        return "CustomAPI"
 
     def schema(self):
+        # NotYetImplemented - self-discovery schema during data load using built-in "schema_of_json" function, ie. schema = f'schema_of_json(response)'
+        # dict[str, str] | StructType 
+        '''
+        self.schema: StructType = schema
+        return "id int, name string, email string, body string"
+            raise PySparkNotImplementedError(
+            error_class="NOT_IMPLEMENTED",
+            message_parameters={"feature": "schema"},
+        )
+        '''
         return "id int, name string, email string, body string"
 
     def streamReader(self, schema: StructType):
-        return CommentsAPIStreamReader(schema, self.options)
+        return CustomAPIStreamReader(schema, self.options)
     
-class CommentsAPIStreamReader(DataSourceStreamReader):
-    def __init__(self, schema, options):
-        self.api_url = options.get("api_url")
-        self.progress_path = options.get("progress_path")
+    def streamWriter(self, schema: StructType):
+        return CustomAPIStreamWriter(schema, self.options)
+
+    def Reader(self, schema: StructType):
+        return CustomAPIReader(schema, self.options)
+    
+    def streamWriter(self, schema: StructType):
+        return CustomAPIWriter(schema, self.options)
+
+class CustomAPIReader(DataSourceReader):
+    pass
+    # raise PySparkNotImplementedError(
+    #         error_class="NOT_IMPLEMENTED",
+    #         message_parameters={"feature": "schema"},
+    #     )
+
+class CustomAPIWriter(DataSourceWriter):
+    pass
+    # raise PySparkNotImplementedError(
+    #         error_class="NOT_IMPLEMENTED",
+    #         message_parameters={"feature": "schema"},
+    #     )
+
+class CustomAPIStreamWriter(DataSourceStreamWriter):
+    pass
+    # raise PySparkNotImplementedError(
+    #         error_class="NOT_IMPLEMENTED",
+    #         message_parameters={"feature": "schema"},
+    #     )
+
+class CustomAPIStreamReader(DataSourceStreamReader):
+    def __init__(self, schema, options, protocol: str = "https", method: str = "GET"):
+        """
+        Initializer of the class. Collects all required parameters.
+
+        Complete URI (ie. "https://${DATABRICKS_HOST}/api/2.0/clusters/get") consists of :
+        * Protocol: default "https"
+        * Host: host-name - literally, everything between "slash" after the protocol name and before the "slash" before API path
+        * Path: target API endoint that we would like to call
+        * Query: query to be pushed agains the give URI to limit the number of records; Parameter present after the "?" sign
+        """
+        self.method = options.get("method", method)
+        self.protocol = options.get("protocol", protocol)
+        self.host = options.get("host")
+        self.path = options.get("path")
+        self.checkpoint_path = options.get("checkpoint_path")
         self.rows_per_batch = options.get("rows_per_batch",10)
-        self.url = f"https://{workspace_url}/api/2.0/fs/files{self.progress_path}progress.json"
+        self.uri = f"{protocol}://{host}/{path}" # self.uri = f"{protocol}://{host}/{path}?{query}"
         self.token = token
+        # self.query = options.get("query", query)
         self._load_progress()
-        
         
     def initialOffset(self) -> dict:
         """
@@ -69,6 +138,8 @@ class CommentsAPIStreamReader(DataSourceStreamReader):
         Plans the partitioning of the current microbatch defined by start and end offset. It
         needs to return a sequence of :class:`InputPartition` objects.
         """
+
+        # RangePartition is part of InputPartition
         return [RangePartition(start["offset"], end["offset"])]
 
     def commit(self, end: dict):
@@ -79,6 +150,7 @@ class CommentsAPIStreamReader(DataSourceStreamReader):
         """
         self._save_progress()
 
+    @abstractmethod 
     def read(self, partition) -> Iterator[Tuple]:
         """
         Takes a partition as an input and reads an iterator of tuples from the data source.
@@ -90,4 +162,15 @@ class CommentsAPIStreamReader(DataSourceStreamReader):
         }
         response = requests.get(self.api_url, params=params)
         for r in response.json():
+        #   for schema = "id int, name string, email string, body string"
+          '''
+          # Functionality to receive 
+          column_names = [field.name for field in schema.fields]
+          if not column_names:
+            return ""
+
+          formatted_columns = [f"r['{col}']" for col in column_names]
+          yield_field = return ", ".join(formatted_columns)
+          yield (f'{yield_field})
+          '''
           yield (r['id'],r['name'],r['email'], r['body'])
